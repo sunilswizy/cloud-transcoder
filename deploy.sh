7c3fd40208c102197afeba3bcab841c49d2c8a26
sunil_swizy <sunilsubash006@gmail.com>
2026-03-01 17:04:23 +0530

    [CI] env inject on docker run

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ docker stop -f cloud-transcoder 2>/dev/null || true
 docker rm -f cloud-transcoder 2>/dev/null || true
 
 echo "Running the new image"
-docker run -d --name cloud-transcoder --restart always $IMG_NAME
+docker run -d --name cloud-transcoder --restart always --env-file /home/ubuntu/cloud-transcoder.env $IMG_NAME
 echo "App deployed successfully"
