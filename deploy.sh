c4ed59901132a0df7c7c558dc57a78bfd5e1f1a0
sunil_swizy <sunilsubash006@gmail.com>
2026-03-01 15:18:43 +0530

    [CI] CI pipeline with github actions

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+IMG_NAME=$1
+
+echo "Pulling the latest image"
+docker pull $IMG_NAME
+
+echo "Stopping old containers"
+docker stop -f cloud-transcoder 2>/dev/null || true
+docker rm -f cloud-transcoder 2>/dev/null || true
+
+echo "Running the new image"
+docker run -d --name cloud-transcoder --restart always $IMG_NAME
+echo "App deployed successfully"
